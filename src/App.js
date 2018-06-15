1812bfa00c3bd45d6429344b09daa33927e50bb9
swiftybathero <swiftybathero@gmail.com>
2018-06-15 22:52:33 +0200

    Changed spacing to 4 spaces

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello ReactJS!
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                Hello ReactJS!
+            </div>
+        );
+    }
 }
 
 export default App;
